e9b353492fa185dee3a5de870d0d5eb040c379c4
Ali <ali7.khosrojerdi@gmail.com>
2025-04-08 19:27:33 +0330

    add: application db context on infrastructure layer

---
+++ Taskino.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskino.Domain.Models.Entities;
+
+namespace Taskino.Infrastructure.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.SetNull;
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<PendUserRegister> PendUserRegisters { get; set; }
+        public DbSet<Domain.Models.Entities.Task> Tasks { get; set; }
+    }
+}
